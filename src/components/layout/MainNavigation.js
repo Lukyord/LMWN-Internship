44391f15480212009b422f0a8fed26d57bc1d02c
Lukyord <ttansiri524@gmail.com>
2022-11-06 15:46:01 +0700

    restore nav bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,15 +2,30 @@ import { Link } from "react-router-dom";
 
 export default function MainNavigation() {
   return (
-    <header>
-      <Link to="/">Where we go?</Link>
+    <header className="flex justify-between w-full h-[5rem] items-center bg-cyan-500 py-2">
+      <Link
+        to="/"
+        className="ml-12 text-4xl font-bold hover:text-white active:text-white focus:text-white"
+      >
+        Where we go?
+      </Link>
       <nav>
-        <ul>
-          <li>
-            <Link to="/all-trips">All Trips</Link>
+        <ul className="flex list-none m-0 p-0 items-baseline">
+          <li className="ml-12">
+            <Link
+              to="/all-trips"
+              className="hover:text-white active:text-white focus:text-white"
+            >
+              All Trips
+            </Link>
           </li>
-          <li>
-            <Link to="/search-trips">Search Trips</Link>
+          <li className="ml-12">
+            <Link
+              to="/search-trips"
+              className="mr-12 hover:text-white active:text-white focus:text-white"
+            >
+              Search Trips
+            </Link>
           </li>
         </ul>
       </nav>
