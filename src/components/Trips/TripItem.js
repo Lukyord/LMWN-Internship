60094cacc6101b991ac988f730fd5d4a75c34966
Lukyord <ttansiri524@gmail.com>
2022-11-08 20:42:16 +0700

    added links to all trips

--- src/components/Trips/TripItem.js
+++ src/components/Trips/TripItem.js
@@ -7,12 +7,20 @@ export default function TripItem(props) {
       <Card>
         <div className="grid grid-cols-10 grid-rows-4">
           <div className="w-full overflow-hidden rounded-bl-md rounded-tl-md row-span-4 col-span-4">
-            <img src={props.photo1} alt={props.title} className="object-fill" />
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={props.photo1}
+                alt={props.title}
+                className="object-fill"
+              />
+            </a>
           </div>
           <div className="text-center p-[1rem] col-span-6 row-span-2">
-            <h3 className="text-2xl text-slate-500 p-[0.5rem]">
-              {props.title}
-            </h3>
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              <h3 className="text-2xl text-slate-500 p-[0.5rem]">
+                {props.title}
+              </h3>
+            </a>
             <p className="p-[0.5rem]">
               <ReadMore>{props.description}</ReadMore>
             </p>
@@ -21,13 +29,31 @@ export default function TripItem(props) {
             </div>
           </div>
           <div className="object-cover overflow-hidden rounded-3xl rounded-b-[3rem] row-span-2 col-span-2 px-[0.5rem]">
-            <img src={props.photo2} alt={props.title} className="object-fill" />
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={props.photo2}
+                alt={props.title}
+                className="object-fill"
+              />
+            </a>
           </div>
           <div className="object-cover overflow-hidden rounded-3xl rounded-b-[3rem] row-span-2 col-span-2 px-[0.5rem]">
-            <img src={props.photo3} alt={props.title} className="object-fill" />
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={props.photo3}
+                alt={props.title}
+                className="object-fill"
+              />
+            </a>
           </div>
           <div className="object-cover overflow-hidden rounded-3xl rounded-b-[3rem] row-span-2 col-span-2 px-[0.5rem]">
-            <img src={props.photo4} alt={props.title} className="object-fill" />
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={props.photo4}
+                alt={props.title}
+                className="object-fill"
+              />
+            </a>
           </div>
         </div>
       </Card>
