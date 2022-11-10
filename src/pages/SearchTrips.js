c4b340f8744b7b4f002eb4e8ef6f2312ad720724
Lukyord <ttansiri524@gmail.com>
2022-11-10 10:20:43 +0700

    added search function

--- src/pages/SearchTrips.js
+++ src/pages/SearchTrips.js
@@ -1,24 +1,87 @@
 import SearchIcon from "@mui/icons-material/Search";
 import TripList from "../components/Trips/TripList";
 
+import { useState } from "react";
+
 import DummyData from "../db-en.json";
 
 export default function SearchTrips(props) {
+  const [filteredData, setFilteredData] = useState([]);
+
+  function seacrhSize(shownResult) {
+    return shownResult.length * 50;
+  }
+
+  function handleFilter(event) {
+    const searchText = event.target.value;
+
+    const newFilter = DummyData.trips.filter((value) => {
+      return value.title.toLowerCase().includes(searchText.toLowerCase());
+    });
+
+    if (searchText === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+
+    // const f = newFilter.map((subArray) =>
+    //   subArray
+    //     .filter((tag) => tag.toLowerCase().includes(enteredText.toLowerCase()))
+    //     .map((tag) => {
+    //       return tag;
+    //     })
+    // );
+  }
+
   return (
-    <div className="flex justify-center content-center h-full pt-[3rem] bg-slate-100">
+    <div className="flex justify-center h-full pt-[3rem] bg-slate-100">
       <div className="flex-col">
         <form>
           <div className="text-xl p-[1rem] flex items-center focus-within:text-gray-600 justify-center">
             <input
               type="text"
               placeholder={props.placeholder}
-              className="border-0 rounded-md rounded-tr-0 rounded-br-0 w-[40rem] p-[1rem] text-black"
+              className="border-0 rounded-md rounded-tr-0 rounded-br-0 w-[40rem] p-[1rem] text-black shadow-2xl"
+              onChange={handleFilter}
             />
             <div className="pl-[36rem] h-4 w-5 absolute mb-5 content-end text-gray-400 pointer-events-none">
               <SearchIcon />
             </div>
           </div>
         </form>
+        {filteredData.length !== 0 && (
+          <div className="ml-[22rem] mt-[5px] w-[40rem] h-[200px] bg-white shadow-2xl overflow-hidden overflow-y-auto">
+            {/* {filteredData.map((subArray) =>
+              subArray
+                .filter((tag) =>
+                  tag.toLowerCase().includes(enteredText.toLowerCase())
+                )
+                .map((tag) => {
+                  return (
+                    <p className="flex items-center text-black h-[50px] pl-[1rem] hover:bg-slate-100">
+                      {tag}
+                    </p>
+                  );
+                })
+            )} */}
+            {filteredData.map((value, key) => {
+              return (
+                <a
+                  href={value.url}
+                  target="_blank"
+                  className="flex items-center text-black h-[50px] pl-[1rem] hover:bg-slate-100"
+                >
+                  <p>
+                    {value.title.length > 80
+                      ? value.title.substr(0, 80) + "..."
+                      : value.title}
+                  </p>
+                </a>
+              );
+            })}
+          </div>
+        )}
         <div>
           <TripList trips={DummyData} />
         </div>
